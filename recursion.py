f09295c5bc491a86c597536b424b255802cbacd8
saint-luci <ese.worldworkshop@mail.ru>
2024-07-24 02:29:18 +0300

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,11 @@
+def get_multiplied_digits(num):
+    str_number = str(num)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+
+result = get_multiplied_digits(40203)
+print(result)
